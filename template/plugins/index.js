d95f27d12632690cba6307262792561c121a2ea7
yongjiancui(崔永键) <yongjiancui@tencent.com>
2019-03-25 15:55:02 +0800

    fix: add plugin-req-logger

--- template/plugins/index.js
+++ template/plugins/index.js
@@ -1,7 +1,12 @@
 module.exports = function(config) {
   return [
     'plugin-global-logger',
-    'plugin-logger',
+    {
+      name: 'plugin-logger',
+      options: {
+        proxy: true // 是否信任代理，信任的话则打印请求时会 X-Forwarded-Host 获取主机名，否则使用 host
+      }
+    },
     'plugin-static',
     'plugin-view',
     'plugin-service',
